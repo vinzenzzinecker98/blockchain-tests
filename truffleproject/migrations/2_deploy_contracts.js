52ea7512c0d71b868d0d8397f19b2e6b9cba95bf
vinzenzzinecker98 <vinzenz@zinecker.de>
2021-03-07 10:34:50 +0000

    remove Metacoin, change migration files

--- truffleproject/migrations/2_deploy_contracts.js
+++ truffleproject/migrations/2_deploy_contracts.js
@@ -1,8 +1,7 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+const FL_simulation = artifacts.require("FL_simulation");
+
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(FL_simulation);
+   
 };
