784fb5b50a32c67739daec4df094cc59ebe39aaa
vinzenzzinecker98 <vinzenz@zinecker.de>
2021-03-07 10:50:08 +0000

    write first tests

---
+++ truffleproject/test/FL_simulation_tests.js
@@ -0,0 +1,22 @@
+const FL_simulation = artifacts.require("FL_simulation");
+
+contract('FL_simulation', (accounts) => {
+    
+  it('should display initial weights', async () => {
+    const FLInstance = await FL_simulation.deployed();
+    const w = await FLInstance.getweights.toNumber();
+
+    assert.equal(w.valueOf(), 0, "initial value was not 0");
+  });
+
+  it('should call the update weights function and then retrieve the updated weights', async () => {
+    const FLInstance = await FL_simulation.deployed();
+    weights=12345;
+    const returncode = (await FLInstance.updateweights.call(weights));
+
+    const retrieve = (await FLInstance.getweights.call().toNumber());
+
+    assert.equal(retrieve, weights, 'retrieved weights do not equal the weights that were put in');
+  });
+ 
+});
