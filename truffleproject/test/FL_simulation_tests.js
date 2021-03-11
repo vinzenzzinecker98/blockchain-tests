2d02dfa213eddeff3c196f69c37cfa7d6a19f253
vinzenzzinecker98 <vinzenz@zinecker.de>
2021-03-11 19:24:22 +0000

    migrate

--- truffleproject/test/FL_simulation_tests.js
+++ truffleproject/test/FL_simulation_tests.js
@@ -1,44 +1,42 @@
 const FL_simulation = artifacts.require("FL_simulation");
 const FL_simple = artifacts.require("FL_simple");
+const CryptoJS = require("crypto-js");
 
 
+
+const rounds = 10;
+const numberaccounts = 5;
+
+var realanswer="seed"
+for (i = 0; i < rounds; i++) {
+  for (account = 0; account < numberaccounts; account++) {
+    realanswer=CryptoJS.SHA256(realanswer).toString(CryptoJS.enc.Hex);
+    
+  }
+}
 
 contract('FL_simple', (accounts) => {
-  it('testing the simple contrract', async () => {
+  it('FL Run', async () => {
 
     const FLInstance = await FL_simple.deployed();
+    var i;
+    var account;
+    var answer;
+    //initiate:
+    await FLInstance.update("seed", { from: accounts[0] });
 
-    await FLInstance.update(123, { from: accounts[0] });
-    const answer = await FLInstance.retrieve()
+    for (i = 0; i < rounds; i++) {
+      for (account = 0; account < numberaccounts; account++) {
+        answer = await FLInstance.retrieve()
 
-    
+        answer = CryptoJS.SHA256(answer).toString(CryptoJS.enc.Hex)
+        
+        await FLInstance.update(answer, { from: accounts[account] });
 
-    assert.equal(answer,123,"test failed")
- });
+      }
+    }
 
-/* 
-
-
-  it('should display initial weights', async () => {
-
-    const FLInstance = await FL_simulation.deployed();
-   const w = await FLInstance.updateweights(123, { from: accounts[0] });
-
-    assert.equal(w.valueOf(), 0, "initial value was not 0"); 
-
-    assert.equal(2,2,"test failed")
+   
+    assert.equal(answer, realanswer, "test failed")
   });
-
-  it('should call the update weights function and then retrieve the updated weights', async () => {
-
-    const FLInstance = await FL_simulation.deployed();
-    weights=12345;
-    const returncode = (await FLInstance.updateweights.call(weights));
-
-    const retrieve = (await FLInstance.getweights.call().toNumber());
-
-    assert.equal(retrieve, weights, 'retrieved weights do not equal the weights that were put in'); 
-    assert.equal(2,2,"test failed")
-  }); */
- 
 });
