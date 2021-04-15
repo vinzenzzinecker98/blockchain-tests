c3390ce15203c544b5b966bffb4e04af25ad2257
vinzenzzinecker98 <vinzenz@zinecker.de>
2021-04-15 17:38:51 +0000

    commenting

--- memorylog.sh
+++ memorylog.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -e
 
 echo "...logging..."
+# add caption
 printf "      date     time $(free -m | grep total | sed -E 's/^    (.*)/\1/g')\n" >> memlog.txt
+
 while true; do
+    #print the ram infos to the logfile
     printf "$(date '+%Y-%m-%d %H:%M:%S') $(free -m | grep Mem: | sed 's/Mem://g')\n" >> memlog.txt
+    #print the cpu load average (processes in queue averaged over 1 minute) into the cpu logfle
     cat /proc/loadavg >> cpulog.txt    
-    #echo "CPU `LC_ALL=C top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}'`% RAM `free -m | awk '/Mem:/ { printf("%3.1f%%", $3/$2*100) }'` HDD `df -h / | awk '/\// {print $(NF-1)}'`" >> cpulog3.txt
+    #print 100 - percentage of the CPU of the Idle process (NF is the last entry of the mpstat command) This command also waits for 1 second - so no wait() is necessary for the loop
     mpstat 1 1 | awk '/all/{ print 100 - $NF; exit; }' >> cpulog2.txt
 done
